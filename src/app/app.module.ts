941df03e4b2874699ce878d80f9848f4103519ce
Marcin Lasak <lasakmarcin90@gmail.com>
2020-01-10 10:03:08 +0100

    Install spartacus with schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '1.3',
+        anonymousConsents: true
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
